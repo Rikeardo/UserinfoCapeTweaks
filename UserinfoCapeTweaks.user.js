e458973c976081f05b6f5a757943c6465aced781
Rikard <rikardhypixel@gmail.com>
2017-02-15 20:24:38 +0100

    Update UserinfoCapeTweaks.user.js

--- UserinfoCapeTweaks.user.js
+++ UserinfoCapeTweaks.user.js
@@ -7,22 +7,5 @@
 // @include     https://goliath.hypixel.net/userinfo#*
 // ==/UserScript==
 
-var uuidUser = $("font").html();
-var username = "";
-if(uuidUser.includes("["))
-{
-    var username = uuidUser.split(" ").pop();
-    while(username.includes("</font>"))
-    {
-        username = username.replace("</font>","");
-    }
-}
-else
-{
-    username = uuidUser;
-}
-$("<img id='cape' src=" + "http://s.optifine.net/capes/" + username + ".png" + " onerror=this.style.display='none'>").insertAfter("img");
-cape_element = document.getElementById("cape");
-cape_element.setAttribute("height", "40%");
-cape_element.setAttribute("width", "40%");
-cape_element.style.margin = "20px";
+var username = /([A-Za-z0-9_]{1,16})$/.exec($("#columnx > font:first-of-type").text())[1];
+$("<img id='cape' style='margin: 20px;' width='40%' src=" + "http://s.optifine.net/capes/" + username + ".png" + " onerror=this.style.display='none'>").insertAfter("img");
